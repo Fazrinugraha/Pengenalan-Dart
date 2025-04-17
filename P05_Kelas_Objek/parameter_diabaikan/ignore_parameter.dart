105df0c9ccddceb9045175a7ac137ebfdffa1f9a
Fazrinugraha <fazrinugraha795@gmail.com>
2025-04-17 19:15:18 +0700

    P5_kelas dan fungsi 2

--- P05_Kelas_Objek/parameter_diabaikan/ignore_parameter.dart
+++ P05_Kelas_Objek/parameter_diabaikan/ignore_parameter.dart
@@ -1,10 +1,10 @@
 class Mahasiswa {
   String name;
-  Function(String name) doingHobby;
+  Function(String name) ? doingHobby;
   Mahasiswa(this.name, {this.doingHobby});
   void takeARest() {
     if (doingHobby != null) {
-      doingHobby(name);
+      doingHobby!(name);
     }
   }
 }
